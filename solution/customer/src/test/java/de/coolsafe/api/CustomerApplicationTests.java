f4bafc2c1ad5d55e316badf9cd219d9b9959ddb9
simon-an <nomisimon@posteo.de>
2019-08-23 10:05:38 +0200

    refactor solution to be a multimodule project

--- solution/customer/src/test/java/de/coolsafe/api/CustomerApplicationTests.java
+++ solution/customer/src/test/java/de/coolsafe/api/CustomerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApiApplicationTests {
+public class CustomerApplicationTests {
 
 	@Test
 	public void contextLoads() {
